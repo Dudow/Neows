15609fb9ec81156de2c9b87d445eaaa5407e210d
Dudow <edu30.nm@gmail.com>
2022-02-04 16:53:51 -0300

    feat: complete structure of application

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Neows - Home</title>
+      </Head>
+
+      <h2>aaa</h2>
+    </>
+  );
+}
